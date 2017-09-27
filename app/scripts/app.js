a9967fa9267c1617980adfde1f76f411088da9b6
Pito Axiu <shpetim.h@gmail.com>
2017-09-27 23:52:40 +0100

    Installed and configured copy and clean modules

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('confusionApp', []).controller('menuController', function() {
   this.tab = 1;
-  this.filtText = ''
+  this.filtText = '';
   
   var dishes = [
     {
