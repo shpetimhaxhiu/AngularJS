2d1d8afc3f78518102c651afe3a098881443469b
Pito Axiu <shpetim.h@gmail.com>
2017-09-29 00:11:42 +0100

    Added Angular Form Validation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -88,9 +88,42 @@ angular.module('confusionApp', [])
         agree:false, 
         email:"" 
     };
+    
+    var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+    
+    $scope.channels = channels;
+    $scope.invalidChannelSelection = false;
+
 }])
 // feedback controller
 .controller('FeedbackController', ['$scope', function($scope) {
+    $scope.sendFeedback = function() {
+        console.log($scope.feedback);
+        
+        if ($scope.feedback.agree 
+            && ($scope.feedback.mychannel == "") 
+            && !$scope.feedback.mychannel) {
+                  $scope.invalidChannelSelection = true;
+                  console.log('incorrect');
+        }
+        else {
+            $scope.invalidChannelSelection = false;
+            $scope.feedback = {
+              mychannel:"", 
+              firstName:"", 
+              lastName:"",
+              agree:false, 
+              email:"" 
+            };
+
+            $scope.feedback.mychannel="";
+
+            $scope.feedbackForm.$setPristine();
+            console.log($scope.feedback);
+        }
+      };
+
+
 
 }])
 
