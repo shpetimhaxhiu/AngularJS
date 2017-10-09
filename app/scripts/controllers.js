d58b72f28c9fa051c5a993e96b7acdc9074c1ab8
Pito Axiu <shpetim.h@gmail.com>
2017-10-09 20:28:41 +0100

    Update to use ng-resource

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -9,26 +9,9 @@ angular.module('confusionApp')
         $scope.showDetails = false;
         $scope.showMenu = false;
         $scope.message = "Loading ...";
-                    $scope.dishes= {};
-                    menuFactory.getDishes()
-        .then(
-            function(response) {
-                $scope.dishes = response.data;
-                $scope.showMenu = true;
-            },
-            function(response) {
-                $scope.message = "Error: "+response.status + " " + response.statusText;
-            }
-        );
 
-        $scope.dishes = [];
-
-        menuFactory.getDishes()
-            .then(
-                function (response) {
-                    $scope.dishes = response.data;
-                }
-            );
+        
+        $scope.dishes = menuFactory.getDishes().query();
 
 
         $scope.select = function (setTab) {
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -2,8 +2,7 @@
 
 angular.module('confusionApp')
         .constant("baseURL","http://localhost:3000/")
-        .service('menuFactory', ['$http', 'baseURL', function($http,baseURL) {
-    
+        .service('menuFactory', ['$resource', 'baseURL', function($resource,baseURL) {
             var promotions = [
                     {
                         _id:0,
@@ -16,12 +15,8 @@ angular.module('confusionApp')
                 ];
 
             this.getDishes = function(){
-                return $http.get(baseURL + "dishes");
-            }
-
-            this.getDish = function (index) {
-                return $http.get(baseURL+"dishes/"+index);
-            }
+                return $resource(baseURL+"dishes/:id",null,  {'update':{method:'PUT' }});
+            };
 
             // implement a function named getPromotion
             // that returns a selected promotion.
